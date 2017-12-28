768a9d7da6301187c99d227db425bf42ad025280
yonghai.yang <yangyonghai@wusongtech.com>
2017-12-28 16:22:18 +0800

    02.rendering a route :melon:

---
+++ lessons/02-rendering-a-route/modules/About.js
@@ -0,0 +1,10 @@
+/**
+ * Created by yang on 2017/12/28.
+ */
+import React from 'react';
+
+export default React.createClass({
+    render() {
+        return <div>about</div>;
+    }
+});
---
+++ lessons/02-rendering-a-route/modules/Repos.js
@@ -0,0 +1,11 @@
+/**
+ * Created by yang on 2017/12/28.
+ */
+import React from 'react';
+
+export default React.createClass({
+    render() {
+        return <div>Crazy</div>;
+    }
+});
+
